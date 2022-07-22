a84bff77bd5fd9d10e7d03c096ca75e5bffeff93
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-07-22 20:06:27 -0300

    FEAT(database): set first model

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.TextField()
+    ein = models.TextField()
+    businessStructure = models.JSONField()
+    address = models.JSONField()
+    cellphone = models.TextField()
+    creationDate = models.DateTimeField('date published')
+    active = models.BooleanField()
+
