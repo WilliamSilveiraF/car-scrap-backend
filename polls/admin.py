34e3ebddf60a6840d4506b3da0690ac39b33b338
williamsilveiraf <williamsilveiraf@hotmail.com>
2022-07-22 21:17:51 -0300

    CHORE(database): set Order model

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+import polls.models as model
+
+
+admin.site.register(model.Company)
+admin.site.register(model.Order)
--- polls/models.py
+++ polls/models.py
@@ -3,9 +3,17 @@ from django.db import models
 class Company(models.Model):
     name = models.TextField()
     ein = models.TextField()
-    businessStructure = models.JSONField()
-    address = models.JSONField()
+    businessStructure = models.JSONField(default=dict)
+    address = models.JSONField(default=dict)
     cellphone = models.TextField()
     creationDate = models.DateTimeField('date published')
     active = models.BooleanField()
+
+class Order(models.Model):
+    product = models.JSONField(default=dict)
+    productAmount = models.IntegerField()
+    #costumer ForeignKey USER
+    total = models.FloatField()
+    issueDate = models.TextField()
+    base64 = models.TextField()
 
